cc785cc01e3021405460176b55a1195d8f5c7c17
cy7372 <caiyu7372@gmail.com>
2026-04-01 12:22:16 +0800

    [Claude Code] Auto backup - DancherLink-qt - 2026-04-01 12:22:16

--- scripts/Build-Common.ps1
+++ scripts/Build-Common.ps1
@@ -213,7 +213,7 @@ cd /d "$CacheFolder"
 echo Running cmake configure...
 del /q CMakeCache.txt
 del /q "%~dp0..\..\..\app\CMakeFiles\DancherLink.dir\flags.make"
-cmake -S "$RootDir" -G "$($BuildConfig.CMakeGenerator)" -DCMAKE_BUILD_TYPE="$($BuildConfig.CMakeBuildType)" -DARCH_DIR="$Arch"$BetaArgs$VersionArg -DUPDATE_SUBSCRIPTION_URL="file:/%20/%20$ReleaseFolder/updates.json" -DOPENSSL_INCLUDE_DIR="$($OpenSslPaths.Inc)" -DOPENSSL_CRYPTO_LIBRARY:FILEPATH="$($OpenSslPaths.Crypto)" -DOPENSSL_SSL_LIBRARY:FILEPATH="$($OpenSslPaths.Ssl)"
+cmake -S "$RootDir" -G "$($BuildConfig.CMakeGenerator)" -DCMAKE_BUILD_TYPE="$($BuildConfig.CMakeBuildType)" -DARCH_DIR="$Arch"$BetaArgs$VersionArg -DUPDATE_SUBSCRIPTION_URL="file://$($ReleaseFolder -replace '\\','/')/updates.json" -DOPENSSL_INCLUDE_DIR="$($OpenSslPaths.Inc)" -DOPENSSL_CRYPTO_LIBRARY:FILEPATH="$($OpenSslPaths.Crypto)" -DOPENSSL_SSL_LIBRARY:FILEPATH="$($OpenSslPaths.Ssl)"
 if %ERRORLEVEL% neq 0 (
     echo CMake configuration FAILED
     goto :cleanup
